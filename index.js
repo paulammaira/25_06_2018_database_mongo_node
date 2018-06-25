af01b6af47d968219ddba6d1deb639dc56c2afc0
Paula Palermo <paulammaira@gmail.com>
2018-06-25 11:26:22 -0300

    adcionamos mais uma coleção para o banco de dados

--- index.js
+++ index.js
@@ -18,8 +18,29 @@ app.get('/churros', (req, res) =>{
     });
 });
 
+app.get('/bebidas', (req, res) =>{
+    req.db.collection('drinks').find().toArray((err, data) => {
+        if(err){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
 app.post('/churro', (req, res) => {
     req.db.collection('sabores').insert(req.body, (err) => {
+        if(err){
+            res.statusMessage(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+        res.send('ok');
+    });
+});
+
+app.post('/bebida', (req, res) => {
+    req.db.collection('drinks').insert(req.body, (err) => {
         if(err){
             res.statusMessage(500).send('Erro ao acessar o banco de dados');
             return;
